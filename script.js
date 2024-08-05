05485966e19e47d81ee603e1421286ae5391dfcc
ahad1512 <139266271+ahad1512@users.noreply.github.com>
2024-08-05 19:45:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+const form = document.getElementById("form1");
+form.addEventListener("submit",getFormvalue);
 function getFormvalue() {
     //Write your code here
-
+	alert(form.fname.value+" "+form.lname.value);
 }
